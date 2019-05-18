ba0965d52910ee288fc081e88f7f4771522ec154
SaTHYa <sathiyamoorthy@theviralfever.com>
2019-05-18 16:23:12 +0530

    Initial Commit

---
+++ ShotsTests/ShotsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShotsTests.swift
+//  ShotsTests
+//
+//  Created by sathiyamoorthy N on 18/05/19.
+//  Copyright © 2019 SaTHYa. All rights reserved.
+//
+
+import XCTest
+@testable import Shots
+
+class ShotsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
